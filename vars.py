0813a324a543b32530c7b1be64590f8b072e1f50
1on1z3r <33895043+1on1z3r@users.noreply.github.com>
2024-04-26 07:26:17 +0200

    Create vars.py
    Primera

---
+++ vars.py
@@ -0,0 +1,47 @@
+
+message_count = {}
+canales = ['#1on1zer', '#patrii19', '#pobrelokopsn']
+exclusiones_usuarios = ['nightbot',
+                        'streamelements',
+                        "comanderroot",
+                        'testigo19',
+                        'moobot',
+                        'patrii19'
+                        ]
+
+chistes_profesores = [
+    "En una escuela la profesora se dirije a un alumno: A ver Juanito, la frase 'yo busco novio', ¿qué tiempo es? Tiempo perdido, señorita.",
+    "La profesora dice:Pablito venía para la escuela, pisó una cáscara de banano y se cayó y se quebró una pierna. ¿Qué hay que aprender de esto Juanito? ¡Que no hay que venir a la escuela!",
+    "En la escuela un niño le pregunta a su maestra: -Profesora, ¿A los cuantos años se muere un burro?. -¿Por qué?, ¿Acaso ya te sientes mal?.",
+    "El profesor estaba en su clase de Química y pregunta: Si introduzco este reloj en esta sustancia, ¿Creen ustedes que se disolvería? Un alumno contestó: No. ¡Muy bien! ¿Por qué? Porque si se disolviera, usted no lo metería.",
+    "Es una verguenza, no te sabes el nombre de los presidentes, a tu edad yo los sabía todos y por orden cronológico. Pues claro maestra, cuando usted tenía mi edad solo habían como tres.",
+    "En una clase preguntan cómo se llamaban las tres naves que llevaron a Colón a América, y en ese momento un alumno ve por la ventana una chica muy guapa y exclama: ¡Santa María, qué pinta tiene la niña!",
+    "Un hombre va a un colegio y pregunta: Me pueden Uds. enseñar a leer y escribir. El director del colegio le dice: por supuesto, lo podemos matricular inmediatamente. Por favor llene el siguiente formulario con todos los datos que se le piden y fírmelo.",
+    "El profesor de Matemáticas: Estoy indignado, más del 80% de la clase no pasó el examen. En eso escucha risas desde la última fila del salón. ¡Si ni siquiera somos tantos!",
+    "Una maestra le dijo al estudiante: ¿Por qué no contestaste ni una pregunta si estaban tan fáciles? Y el estudiante responde: Pero las respuestas no.",
+    "Alumno: ¿Qué significa 'higher'? Profesor: Más alto Alumno: ¿QUE SIGNIFICA HIGHER?",
+    "A ver Pepito . ¿dónde está el sujeto en esta oración.? El enriqueció de la noche a la mañana. Pepito: Seguramente en la política señorita.",
+    "El alumno al profesor: Señor profesor por qué se dice 'chespir' y se escribe Shakespeare. Está muy claro hombre, porque es un verbo irregular.",
+    "Jaimito le pregunta a la profesora de inglés: Profesora, que significa ¿why? -Por qué. -Porque quiero saberlo…",
+    "Dos amigos conversando.. ¿Te acuerdas de aquel profesor que nos decía que jamas llegaríamos a ser nada en la vida ? ¡Realmente era un visionario el profesor!",
+    "Hijo ¿qué tal hoy en el colegio ? mal mamá , muy mal. Deben haber unos ocho profesores que me tienen manía y me castigan.",
+    "El profesor le hace un examen de historia y le pregunta a Nabucodonosor, 'Dígame qué le dijo Bolívar al coronel Rondón en la batalla de boyacá?' Nabucodonosor responde, 'Lo siento pero mi mamá me ha dicho que no me meta en conversaciones ajenas.'"
+
+]
+
+
+
+
+
+
+
+
+
+
+
+# msyql
+server = 'PMYSQL143.dns-servicio.com'
+usuario = 'daniel'
+clave = '*ek0vA979'
+db = '8267796_dani'
+datosConexion = {"host": server, "port": 3306, "user": usuario, "passwd": clave, "db": db}
